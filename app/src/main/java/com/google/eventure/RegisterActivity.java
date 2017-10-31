e60a1fa8debfd19a10b1ff4b98eda86e93de5b2f
Marylou <mrobles7@ucmerced.edu>
2017-10-31 11:40:24 -0700

    Created Login/Register Activity with LoginActivity having a Register link that switches to RegisterActivity when clicked on. Changed AndroidManifest.xml file to allow Login and Register to emulate over activity_main.xml. Placed Infer Constraints on Login/Register Activity to prevent layout mishaps. Deleted unused .xml file.

---
+++ app/src/main/java/com/google/eventure/RegisterActivity.java
@@ -0,0 +1,25 @@
+package com.google.eventure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        //Hold a reference to all the activities in the XML file
+        //Its a final variable because editName is the only one its gonna be assigned to
+        //FindViewById looks at the activity register filed and finds the id
+        final EditText editName = (EditText)findViewById(R.id.editName);
+        final EditText editUsername = (EditText)findViewById(R.id.editUsername);
+        final EditText editPassword = (EditText)findViewById(R.id.editPassword);
+        final Button buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
+    }
+}
