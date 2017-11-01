61929e056810168d0f9b2f2a360621b7a5d365bf
ReignNelson <reignnelson@yahoo.com>
2017-11-01 14:41:45 -0700

    created an example student with 1 class and added it to database

--- app/src/main/java/com/google/eventure/RegisterActivity.java
+++ app/src/main/java/com/google/eventure/RegisterActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+
 public class RegisterActivity extends AppCompatActivity
 {
+    //initialize database
+    DBHandler db;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -21,5 +24,27 @@ public class RegisterActivity extends AppCompatActivity
         final EditText editPassword = (EditText)findViewById(R.id.editPassword);
         final Button buttonRegister = (Button) findViewById(R.id.buttonRegister);
 
+        //Convert to Edittext string then add to database
+        String FirstName =  editName.toString();
+        String Username =  editUsername.toString();
+        String Passwword =  editPassword.toString();
+        db = new DBHandler(getApplicationContext());
+
+        //creating a student object
+        Student Student1 = new Student(FirstName,Username,Passwword,FirstName);
+
+        //random class
+        Event Bio = new Event("BIO", "COB120","BIO Class","M W F 12:30-1:20");
+        //Insert class into database
+        long Event_id = db.createEvent(Bio);
+
+        //insert student in database with bio class
+        long Student1_id = db.createStudent(Student1,new long[]{Event_id});
+
+
+        /*
+        when create event/add event are done i will link each student to events but
+        for now just creating students with no events in database
+         */
     }
 }
