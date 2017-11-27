d0b230326c80fcc886488466c6c246e450bd4062
ReignNelson <reignnelson@yahoo.com>
2017-11-27 12:05:06 -0800

    added the current date to schedule activity and the next and prev buttons

--- app/src/main/java/com/google/eventure/ScheduleActivity.java
+++ app/src/main/java/com/google/eventure/ScheduleActivity.java
@@ -2,8 +2,11 @@ package com.google.eventure;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import java.util.Calendar;
+import android.widget.TextView;
 
+import java.util.Calendar;
+import java.text.*;
+import java.util.Date;
 
 public class ScheduleActivity extends AppCompatActivity {
 
@@ -11,5 +14,19 @@ public class ScheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+
+        final TextView Date = (TextView) findViewById(R.id.date);
+
+
+        String currentDateString = DateFormat.getDateInstance().format(new Date());
+
+        // textView is the TextView view that should display it
+        Date.setText(currentDateString);
+
+
     }
+
+
+
 }
