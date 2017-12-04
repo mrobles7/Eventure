83976082552e10010475ba5c1f1f08c79be9e8e7
ReignNelson <reignnelson@yahoo.com>
2017-12-03 17:26:35 -0800

    added a function in Event suggest that returns a list of all events linked to the student passed into it

--- app/src/main/java/com/google/eventure/EventSuggest.java
+++ app/src/main/java/com/google/eventure/EventSuggest.java
@@ -1,12 +1,31 @@
 package com.google.eventure;
 
 import java.util.List;
-
-
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.List;
 /**
  * Created by LukeMooney on 12/3/2017.
  */
 
-public class EventSuggest {
+public class EventSuggest extends DatabaseHelper {
+
+
+    public EventSuggest(Context context) {
+        super(context);
+    }
+
+    List <Event> GetallEvents(Student student) {
+
+        return getAllEventsByStudent(student.getPassword());
+
+    }
 
 }
+
+
+
