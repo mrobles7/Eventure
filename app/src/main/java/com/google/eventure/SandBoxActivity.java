b5c7b939ddba1dffec6342fa45440f958e481186
ReignNelson <reignnelson@yahoo.com>
2017-12-09 22:26:52 -0800

    Implemented cards class and have them pop up dynamically

--- app/src/main/java/com/google/eventure/SandBoxActivity.java
+++ app/src/main/java/com/google/eventure/SandBoxActivity.java
@@ -1,16 +1,95 @@
 package com.google.eventure;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
-public class SandBoxActivity extends AppCompatActivity
-{
+import java.util.List;
+
+
+public class SandBoxActivity extends LoginActivity {
+
+    LinearLayout mRelativeLayout;
+
+    private Context mContext;
+    @SuppressLint("WrongViewCast")
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
+        // Request window feature action bar
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sand_box);
 
+        Event Event1 = new Event(1,2017,12,5,3,30,5,30,0,
+                0,"Final class","BIO","COB2");
+        Event Event2 = new Event(2,2017,12,6,3,30,6,0,0,
+                0,"Final class","ART","COB2");
+        long Event1_id = db.createEvent(Event1, new long[]{student.getID()});
+        long Event2_id = db.createEvent(Event2, new long[]{student.getID()});
+        // Get the application context
+        mContext = getApplicationContext();
+
+        // Change the action bar color
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.BLUE));
+
+        // Get the widgets reference from XML layout
+        mRelativeLayout = (LinearLayout) findViewById(R.id.rl_Container);
+        int height =-2;
+
+
+
+        DatabaseHelper db;
+        db = new DatabaseHelper(getApplicationContext());
+        List<Event> events = db.getAllEventsByStudent( student.getPassword() );
+        for (Event event : events) {
+
+            // Initialize a new CardView
+            CardView card = new CardView(mContext);
+
+            // Set the CardView layoutParams
+            LayoutParams params = new LayoutParams(LayoutParams.WRAP_CONTENT, height-=2);
+            card.setLayoutParams(params);
+
+            // Set CardView corner radius
+            card.setRadius(9);
+
+            // Set cardView content padding
+            card.setContentPadding(15, 15, 15, 15);
+
+            // Set a background color for CardView
+            card.setCardBackgroundColor(Color.parseColor("#FFC6D6C3"));
+
+            // Set the CardView maximum elevation
+            card.setMaxCardElevation(15);
+
+            // Set CardView elevation
+            card.setCardElevation(9);
+
+            // Initialize a new TextView to put in CardView
+            TextView tv = new TextView(mContext);
+            tv.setLayoutParams(params);
+            tv.setText(event.getName());
+            tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 30);
+            tv.setTextColor(Color.RED);
+
+            // Put the TextView in CardView
+            card.addView(tv);
+
+            // Finally, add the CardView in root layout
+            mRelativeLayout.addView(card);
+
+        }
     }
 }
